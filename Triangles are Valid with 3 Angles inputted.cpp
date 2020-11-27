cecc49ad4ba311b8076efa428747d8c95dd18f58
Jamaica Loraine Anhtonette I. Dumatal <73677163+jamaicadumatal12@users.noreply.github.com>
2020-11-27 11:36:33 +0800

    Add files via upload

---
+++ Triangles are Valid with 3 Angles inputted.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+		int a,b,c,n;
+		printf("\n enter the three angles");
+		scanf("\n %d%d%d" ,&a,&b,&c);
+		n=a+b+c;
+		if(n==160)
+		
+					printf("\n valid");
+			else
+					printf("\n not valid") ;
+					
+}
