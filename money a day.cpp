0985622eeb7cac7e403a7bfe5c7f7f4d3bbed79d
Jamaica Loraine Anhtonette I. Dumatal <73677163+jamaicadumatal12@users.noreply.github.com>
2020-12-05 21:34:33 +0800

    Add files via upload

---
+++ money a day.cpp
@@ -0,0 +1,14 @@
+// money a day
+#include <stdio.h>
+int main ()
+{
+	int i;
+	double sum = 0.10;
+	
+	i=2; 1 <=15; i++;
+	{
+		sum = sum + sum;
+		printf("Total of money for day %d is %.2f\n",i, sum);
+	}
+	return 0;
+}
